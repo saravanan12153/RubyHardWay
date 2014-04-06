a77f5e3c98a551a6fac1f02410f6cf523aabcf89
Steve Musgrave <steve.musgrave@yahoo.co.uk>
2014-04-06 20:22:35 +0100

    more file operations included

--- ex15.rb
+++ ex15.rb
@@ -1,15 +1,29 @@
+# gets the first file name form the argument passed to the program
+# on the command line
 filename = ARGV.first
 
+
 prompt = "> "
+# the variable txt is made to open the file
 txt = File.open(filename)
 
-puts "Here's your file: #{filename}"
+puts; puts "Here's your file: #{filename}"
+
+#prints the contents of the file to screen
 puts txt.read()
+# check if the file txt is closed
+puts txt.closed?
+# close the file txt
+txt.close
+# check if the file txt is closed
+puts txt.closed?
+# puts "I'll ask you to type it again:"
+# print prompt
 
-puts "I'll ask your to ype it again:"
-print prompt
-file_again = STDIN.gets.chomp()
+# # gets a filename from the user 
+# file_again = STDIN.gets.chomp()
 
-txt_again = File.open(file_again)
+# txt_again = File.open(file_again)
 
-puts txt_again.read()
+# puts txt_again.read()
+puts
